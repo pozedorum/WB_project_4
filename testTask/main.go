50d24eecc0da510b451a1d3672dbf9cadd0c166e
poseidon200500 <poseidon200500@yandex.ru>
2025-10-06 19:24:07 +0300

    промежуточный коммит

---
+++ testTask/main.go
@@ -0,0 +1,24 @@
+package testtask
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/pozedorum/WB_project_4/or"
+)
+
+func main() {
+	ch1 := make(chan any)
+	ch2 := make(chan any)
+	orChan := or.Or(ch1, ch2)
+
+	// В другой горутине
+	go func() {
+		time.Sleep(time.Second)
+		close(ch1)
+	}()
+
+	// Будет получено значение, когда закроется ch1
+	<-orChan
+	fmt.Println("One of the channels closed!")
+}
